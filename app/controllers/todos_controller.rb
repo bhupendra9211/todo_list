d87fd41a823d75cc0cbd64c5af5aba15becfbac0
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-28 13:08:47 +0545

    Adding create method for saving task

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,26 @@
 class TodosController < ApplicationController
     def index
-        @list=Todo.all
+        @task=Todo.all
     end
 
     def new
-        @list=Todo.new
+        @task=Todo.new
+    end
+
+    def create
+        @task = Todo.new(task_params)
+        if @task.save 
+            redirect_to todos_path
+        else
+            render :new
+        end
+    end
+
+
+    private
+
+    def task_params
+        params.require(:todo).permit(:task)
     end
 
 
